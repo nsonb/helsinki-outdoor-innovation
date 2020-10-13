736a707dd7a34d8fe9d209e69af0caa265d32c80
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-10-14 01:48:13 +0300

    Update SearchComponent.js

--- src/component/SearchComponent.js
+++ src/component/SearchComponent.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Search = () => {
+    const [term, setTerm] = useState('');
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(term);
+    }
+
     return (
-        <div>Search</div>
+        <div>
+            <label>Search for location</label>
+            <form onSubmit={onSubmit}>
+                <input type='text' value={term} onChange = {(e)=> setTerm(e.target.value) }/>
+            </form>
+            
+        </div>
     )
 }
 
