424cb01c02c28b8747c436b99878d0c69f5d1b09
LottaL <lotta.s.laukkanen@gmail.com>
2020-10-22 20:55:33 +0300

    starting the map view with leaflet

---
+++ src/component/MapMain.js
@@ -0,0 +1,16 @@
+import React, {useState, useContext, useEffect} from 'react';
+import { SportsContext } from '../Contexts/SportsContexts';
+
+const Search = () => {
+    const { searchOneSport } = useContext(SportsContext);
+
+
+    
+    return (
+        <div className='map-container'>
+
+        </div>
+    )
+}
+
+export default Search;
