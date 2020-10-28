4952196d5357e37bc18ff4167e99bed432637d8d
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-10-29 01:10:55 +0200

    added arrow function and beautification for image

--- src/component/ImageHolder.js
+++ src/component/ImageHolder.js
@@ -5,22 +5,34 @@ import './ImageHolder.css';
 // accept an array of images sources as props, preferably handling 3 - 4 images
 
 const ImageHolder = ({images}) => {
-    const [display, setDisplay] = useState(images[0]);
+    const [displayIndex, setDisplayIndex] = useState(0);
+    console.log(images)
 
     useEffect(() => {
         
-    }, [display]);
+    }, [displayIndex]);
 
-    const renderedButtons = images.map((img) => {
+    const renderedButtons = images.map((img, index) => {
+        const opacity = index === displayIndex? 0.8 : 0.3
         return (
-            <div className='button' key={img} onClick = {() => {setDisplay(img)}}></div>
+            <div className='button' key={img} onClick = {() => {setDisplayIndex(index)}} style={{opacity:opacity}}></div>
         )
     })
 
+    const setImage = (value) => {
+        if(value === true) {
+            (displayIndex + 1) >= images.length ? setDisplayIndex(0) : setDisplayIndex(displayIndex+1)
+        } else {
+            (displayIndex - 1) <= 0 ? setDisplayIndex(images.length-1) : setDisplayIndex(displayIndex-1)
+        }
+    }
+
     return (
         <div className = 'container'>
+            <div className='left-button' onClick={() => setImage(true)}></div>
+            <div className='right-button' onClick={() => setImage(false)}></div>
             <div>
-                <img className = 'top-img'src = {display} alt='depicting season'/>
+                <img className = 'top-img'src = {images[displayIndex]} alt='depicting season'/>
             </div>
             <div className = 'bottom-row'>
                 {renderedButtons}
