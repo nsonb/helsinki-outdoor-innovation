5660d7ab24479dbb4e64bc5099dd0ad7116b852c
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-10-29 23:12:55 +0200

    Refactoring

--- src/component/DetailedInfoModal.js
+++ src/component/DetailedInfoModal.js
@@ -5,33 +5,33 @@ import React from 'react';
 
 const DetailedInfoModal = ({location, closeModal}) => {
 
-        // Styles for the page
-        const modal = {
-            position: 'fixed',
-            display: 'block',
-            overflow: 'auto',
-            width: '100%',
-            height: '100%',
-            top: '0',
-            left: '0',
-            right: '0',
-            bottom: '0',
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            zIndex: '999',
-            cursor: 'default',
+    // Styles for the page
+    const modal = {
+        position: 'fixed',
+        display: 'block',
+        overflow: 'auto',
+        width: '100%',
+        height: '100%',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        zIndex: '999',
+        cursor: 'default',
 
-        }
-        const content = {
-            margin: '15% auto',
-            backgroundColor: 'white',
-            borderRadius: '0.5rem',
-            width: '50vw',
-            padding: '2rem',
-            position: 'relative',
-        }
-        const placeName = {
-            fontSize: '30px',
-        }
+    }
+    const content = {
+        margin: '15% auto',
+        backgroundColor: 'white',
+        borderRadius: '0.5rem',
+        width: '50vw',
+        padding: '2rem',
+        position: 'relative',
+    }
+    const placeName = {
+        fontSize: '30px',
+    }
     const closeButton = () => (
         <button
             onClick={closeModal}
@@ -47,18 +47,18 @@ const DetailedInfoModal = ({location, closeModal}) => {
             }}
         >Close</button>
     );
-        return (
-                <div style={modal}>
-                    <div style={content}>
-                        { closeButton() }
-                        <h1 style={placeName}>{location.name_en || location.name_fi || location.name_sv}</h1>
-                        <p>{location.street_address_fi}</p>
-                        <img src={""} alt={""}/>
-                        <p>{location.short_desc_fi || location.short_desc_sv}</p>
-                        <p>Information</p>
-                        <button onClick={() => console.log(location)}>Find route</button>
-                    </div>
-                </div>
-        );
-    }
+    return (
+        <div style={modal}>
+            <div style={content}>
+                {closeButton()}
+                <h1 style={placeName}>{location.name_en || location.name_fi || location.name_sv}</h1>
+                <p>{location.street_address_fi}</p>
+                <img src={""} alt={""}/>
+                <p>{location.short_desc_fi || location.short_desc_sv}</p>
+                <p>Information</p>
+                <button onClick={() => console.log(location)}>Find route</button>
+            </div>
+        </div>
+    );
+}
 export default DetailedInfoModal;
