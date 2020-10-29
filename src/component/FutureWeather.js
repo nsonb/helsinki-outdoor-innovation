b3f146ca2f4c9e7659e6a507aa4fb02e9a660e5f
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-10-29 17:06:58 +0200

    added date of week

--- src/component/FutureWeather.js
+++ src/component/FutureWeather.js
@@ -15,12 +15,21 @@ const FutureWeather = ({weather}) => {
             img=rain;
             break;
     }
+    var d = new Date(weather.time.slice(0,10));
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
 
     return (
     <div className='future-weather-item'>
         <img className='background-img' src={img} alt = ''/>
         <div className='info-container'>
-            <div>{weather.time.slice(0,10)}</div>
+            <div>{weekday[d.getDay()]}</div>
             <div>{weather.temp}</div>
             <div>{weather.feelsLike}</div>
             <div>{weather.weatherDesc}</div>
