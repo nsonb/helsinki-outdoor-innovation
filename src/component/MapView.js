02abd255a89d348f063dba6a86c3be7a85f9d2fa
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-11-02 21:18:43 +0200

    adjusted to new map

--- src/component/MapView.js
+++ src/component/MapView.js
@@ -5,7 +5,7 @@ const MapView = () => {
   return (
     <MapContainer center={[60.19, 24.94]} zoom={12}>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
