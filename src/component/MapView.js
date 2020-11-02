d2c8b226623817816ea8ac12782c8f083c75542c
Matias Hynnä <38789985+matiaseh@users.noreply.github.com>
2020-11-02 18:13:25 +0200

    Update the map layer link

--- src/component/MapView.js
+++ src/component/MapView.js
@@ -3,10 +3,17 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 const MapView = () => {
   return (
+    
     <MapContainer center={[60.19, 24.94]} zoom={12}>
+
+
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        id='mapbox/streets-v11'
+        accessToken= 'pk.eyJ1IjoibWF0aWFzZWgiLCJhIjoiY2tncWRsend4MDQ1ODJycXdwMmJ4MzZ6dCJ9.mvbD4fDb8KGLjXhhLy0XSA'
+
       />
     </MapContainer>
   );
