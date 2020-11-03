587c76886e87f72231d8bb5bd0e8610515515a49
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-11-03 14:59:46 +0200

    styling miniweather

--- src/component/MiniWeather.js
+++ src/component/MiniWeather.js
@@ -12,11 +12,12 @@ const MiniWeather = () => {
     }
 
     return (
-        <div className='weather-container secondary-background-color' onClick={() => history.push('/weather')}>
-            <div className = 'weather-element'>temperature: {currWeather.temp}</div>
+        <div className='weather-container main-background-color-faded' onClick={() => history.push('/weather')}>
+            <div className = 'weather-element'>temp {currWeather.temp}</div>
+            <div className = 'weather-element'>feels like {currWeather.feelsLike}</div>
             <div className = 'weather-element'>wind: {currWeather.wind}</div>
             <div className = 'weather-element'>humidity: {currWeather.humidity}</div>
-            <div className = 'weather-element'>weather: {currWeather.weather}</div>
+            <div className = 'weather-element'> {currWeather.weatherDesc} </div>
         </div>
     )
 }
