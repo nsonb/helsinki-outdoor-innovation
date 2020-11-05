4d2d25f5d4dddedd40b9f7eafe4e28989e54ffa9
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-11-05 15:12:31 +0200

    added back button

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CurrentWeather from '../component/CurrentWeather';
 import FutureWeatherHolder from '../component/FutureWeatherHolder';
+import BackButton from '../component/BackButton';
 import './WeatherPage.css';
 
 
 const WeatherPage = () => {
     return (
     <div>
+        <BackButton />
         <CurrentWeather />
         <FutureWeatherHolder />
     </div>
