3fbfa8704e1c5c99ba17f490dbfb57eb2a865d88
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-12-08 10:26:48 +0200

    removed unnecessary files

--- src/Scripts/waterTempAPI.js
+++
@@ -1,8 +0,0 @@
-export const getWaterTemp = () => {
-    const apiUrl = 'https://iot.fvh.fi/opendata/uiras/'
-    return fetch(apiUrl)
-        .then((res) => res.json())
-        .then((result) => {
-            return result;
-        });
-}
